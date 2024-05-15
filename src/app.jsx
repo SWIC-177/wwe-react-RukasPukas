c300d8b887ee850ace7273236e8faf1a18698f01
unknown <lukastyon@gmail.com>
2024-05-14 22:45:00 -0500

    Import search, initialize search component

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,10 @@
+import Search from "./components/search";
 import Table from "./components/table";
 import { BELTS, CHAMPIONS } from "./data";
 
 export default function App() {
   return (
+    <Search />
     <Table
       col1={BELTS}
       col2={CHAMPIONS}
