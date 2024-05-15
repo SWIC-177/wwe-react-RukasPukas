1bc223cbb70576dad7c4813c06a39616a388c0ac
unknown <lukastyon@gmail.com>
2024-05-14 22:53:50 -0500

    Remove fragment, update with div class and attributes

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,10 @@
-import { Fragment } from "react";
 import Search from "./components/search";
 import Table from "./components/table";
 import { BELTS, CHAMPIONS } from "./data";
 
 export default function App() {
   return (
-    <Fragment>
+    <div className="container mx-auto p-8 text-center">
       <Search />
       <Table
         col1={BELTS}
@@ -13,6 +12,6 @@ export default function App() {
         col1Header="Belt"
         col2Header="Champion"
       />
-    </Fragment>
+    </div>
   );
 }
