0f47c15816002d802ce5306657d00882a2311378
unknown <lukastyon@gmail.com>
2024-05-14 23:08:58 -0500

    Import filter itemsByTerm

--- src/app.jsx
+++ src/app.jsx
@@ -2,9 +2,17 @@ import Search from "./components/search";
 import Table from "./components/table";
 import { BELTS, CHAMPIONS } from "./data";
 import { useState } from "react";
+import { filterItemsByTerm } from "./utils";
 
 export default function App() {
   const [searchTerm, setSearchTerm] = useState("");
+
+  const filteredResults = filterItemsByTerm({
+    terms1: this.#champions,
+    terms2: this.#belts,
+    actionTerm: this.#search,
+  });
+
   return (
     <div className="container mx-auto p-8 text-center">
       <Search />
