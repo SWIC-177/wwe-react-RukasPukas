7521a3c0c0c5e555fbfa7e8103285f0484e3118e
unknown <lukastyon@gmail.com>
2024-05-14 22:47:33 -0500

    Import fragment from react, encapsulate components in fragment

--- src/app.jsx
+++ src/app.jsx
@@ -1,15 +1,18 @@
+import { Fragment } from "react";
 import Search from "./components/search";
 import Table from "./components/table";
 import { BELTS, CHAMPIONS } from "./data";
 
 export default function App() {
   return (
-    <Search />
-    <Table
-      col1={BELTS}
-      col2={CHAMPIONS}
-      col1Header="Belt"
-      col2Header="Champion"
-    />
+    <Fragment>
+      <Search />
+      <Table
+        col1={BELTS}
+        col2={CHAMPIONS}
+        col1Header="Belt"
+        col2Header="Champion"
+      />
+    </Fragment>
   );
 }
